eb653153bfc766cc2071efbb4de3517a6448a9fa
sufiyanjunaidi <Sufiyan.junaidi@gmail.com>
2023-06-08 00:55:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,53 @@
 const express = require('express');
 const app = express();
-const fs = require('fs/promises');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 
-// Parse JSON request body
-app.use(bodyParser.json());
+// Read user data from JSON file
+let userData = JSON.parse(fs.readFileSync('users.js'));
 
-app.get('/listUsers', async (req, res) => {
-  try {
-    const data = await fs.readFile(__dirname + '/users.json', 'utf8');
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
+// API endpoint to retrieve a list of users
+app.get('/users', (req, res) => {
+  res.json(userData);
+});
+
+// API endpoint to add a new user
+app.post('/addUser', (req, res) => {
+  const newUser = req.body;
+  userData[`user${Object.keys(userData).length + 1}`] = newUser;
+  saveUserData();
+  res.send('User added successfully!');
+});
+
+// API endpoint to get a user by ID
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  const user = userData[`user${userId}`];
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found!');
   }
 });
 
-app.get('/:id', async (req, res) => {
-  try {
-    const data = await fs.readFile(__dirname + '/users.json', 'utf8');
-    const users = JSON.parse(data);
-    const user = users[`user${req.params.id}`];
-    if (user) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(user);
-    } else {
-      res.status(404).send('User not found');
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
+// API endpoint to delete a user by ID
+app.delete('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  const user = userData[`user${userId}`];
+  if (user) {
+    delete userData[`user${userId}`];
+    saveUserData();
+    res.send('User deleted successfully!');
+  } else {
+    res.status(404).send('User not found!');
   }
 });
 
-app.post('/addUser', async (req, res) => {
-  try {
-    const data = await fs.readFile(__dirname + '/users.json', 'utf8');
-    const users = JSON.parse(data);
-    const newUser = req.body;
-    const userId = `user${Object.keys(users).length + 1}`;
-    users[userId] = newUser;
-    await fs.writeFile(__dirname + '/users.json', JSON.stringify(users, null, 2), 'utf8');
-    res.status(200).send('User added successfully');
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// Helper function to save user data to the JSON file
+function saveUserData() {
+  fs.writeFileSync('users.js', JSON.stringify(userData, null, 2));
+}
 
-app.delete('/deleteUser/:id', async (req, res) => {
-  try {
-    const data = await fs.readFile(__dirname + '/users.json', 'utf8');
-    const users = JSON.parse(data);
-    const user = users[`user${req.params.id}`];
-    if (user) {
-      delete users[`user${req.params.id}`];
-      await fs.writeFile(__dirname + '/users.json', JSON.stringify(users, null, 2), 'utf8');
-      res.status(200).send('User deleted successfully');
-    } else {
-      res.status(404).send('User not found');
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-const server = app.listen(8081, () => {
-  const { address, port } = server.address();
-  console.log(`Server running at http://${address}:${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
