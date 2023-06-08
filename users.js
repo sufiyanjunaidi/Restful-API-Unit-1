d7a760d5ef6b282cb40a0b1d22bdf8b9c41e8deb
sufiyanjunaidi <Sufiyan.junaidi@gmail.com>
2023-06-08 00:52:21 -0500

    create users.js

---
+++ users.js
@@ -0,0 +1,20 @@
+{
+    "user1": {
+        "name": "sufiyan",
+        "password": "password1",
+        "profession": "Data Engineer",
+        "id": 1
+    },
+    "user2": {
+        "name": "virat",
+        "password": "password2",
+        "profession": "cricketer",
+        "id": 2
+    },
+    "user3": {
+        "name": "rohit",
+        "password": "password3",
+        "profession": "pilot",
+        "id": 3
+    }
+}
